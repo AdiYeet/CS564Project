e79009d819e1736c63a4bcc08d7f9f60ae4c1011
KJ-11 <113939507+KJ-11@users.noreply.github.com>
2023-08-02 15:31:58 +0400

    fixing earlier commit and placing files in correct directory

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-  public static void main(String[] args) {
-
-    LoginPage loginPage = new LoginPage(); // instantiate loginpage object
-    loginPage.setup(); // start up application
-  }
-}
